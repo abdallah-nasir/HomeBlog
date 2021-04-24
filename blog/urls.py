5e03e3bd0fb4db42ee49c2bb7e5f03075fbc054a
abdallah nasir <abdallah.nasir@ymail.com>
2021-04-24 16:48:53 +0200

    commit-1

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path,include
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",include("BlogApp.urls",namespace="home")),
+    path("marketing/",include("marketing.urls",namespace="marketing")),
+    #this is the TextEditor
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    #Django allauth
+    path('accounts/', include('allauth.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
